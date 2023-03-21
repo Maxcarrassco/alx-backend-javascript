0ca26c13b0e597f568227a5b7eb8a261b730fbff
Maxwell D. Dorliea <deboy170@gmail.com>
2023-03-21 01:51:04 +0000

    Added a function that returns an iterator for employee object

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,7 @@
+export default function createIteratorObject (report) {
+  return (function * _ () {
+    for (const obj of Object.values(report.allEmployees)) {
+      for (const empl of obj) { yield empl; }
+    }
+  }());
+}
