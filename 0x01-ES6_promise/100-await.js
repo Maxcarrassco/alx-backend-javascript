a15bd22ad417efd7c813669dc0b01ab38eef733d
Maxwell D. Dorliea <deboy170@gmail.com>
2023-03-22 23:11:13 +0000

    Resolve promise with await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,12 +2,10 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    let photo = await uploadPhoto();
-    let user = await createUser();
-    photo = await photo.json();
-    user = await user.json();
+    const photo = await uploadPhoto();
+    const user = await createUser();
     return { photo, user };
-  } catch (_) {
+  } catch (err) {
     return {
       photo: null,
       user: null,
