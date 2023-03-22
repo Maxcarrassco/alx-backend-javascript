af87e6950f5b3c90f4f8679e1cb0bdf548caec1f
Maxwell D. Dorliea <deboy170@gmail.com>
2023-03-22 22:05:03 +0000

    Settle all promises using promise.allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise = Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  return promise.then((res) => ({ status: res.status, value: res }))
-    .catch((err) => ({ status: err.status, value: err }));
+  const promise = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return promise.then((res) => res.map((obj) => ({
+    status: obj.status, value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
+  })));
 }
