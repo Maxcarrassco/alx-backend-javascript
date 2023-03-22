ac95930fd0eb403e2752fd337095c08eb9c08a5a
Maxwell D. Dorliea <deboy170@gmail.com>
2023-03-22 21:18:43 +0000

    Resolve array of promises using Promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const promise = Promise.all([uploadPhoto, createUser]);
-  promise.then((data) => {
+  return promise.then((data) => {
     console.log(data[0].body, data[1].firstName, data[1].lastName);
   }).catch(() => console.log('Signup system offline'));
 }
