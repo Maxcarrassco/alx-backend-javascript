43df71dcd4e12d5d0fea3821a32472fd8951ad17
Maxwell D. Dorliea <deboy170@gmail.com>
2023-03-23 23:33:20 +0000

    Create a class called Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,38 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._code = code; // eslint-disable-line
+    this._name = name; // eslint-disable-line
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = name; // eslint-disable-line
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    this._code = code; // eslint-disable-line
+  }
+
+  get name() {
+    return this._name; // eslint-disable-line
+  }
+
+  get code() {
+    return this._code; // eslint-disable-line
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code; // eslint-disable-line
+  }
+}
