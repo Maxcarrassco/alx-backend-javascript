3018067c13a7c0b02ac72675604cccb58bfbeefe
Maxwell D. Dorliea <deboy170@gmail.com>
2023-03-23 23:17:37 +0000

    Fixed a typo in task 6 solution

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,26 +1,26 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor(sqft, floor) {
+  constructor(sqft, floors) {
     super(sqft);
-    if (typeof floor !== 'number') {
+    if (typeof floors !== 'number') {
       throw TypeError('Floor must be a number');
     }
-    this._floor = floor; // eslint-disable-line
+    this._floors = floors; // eslint-disable-line
   }
 
-  set floor(floor) {
-    if (typeof floor !== 'number') {
+  set floors(floors) {
+    if (typeof floors !== 'number') {
       throw TypeError('Floor must be a number');
     }
-    this._floor = floor; // eslint-disable-line
+    this._floors = floors; // eslint-disable-line
   }
 
-  get floor() {
-    return this._floor // eslint-disable-line
+  get floors() {
+    return this._floors // eslint-disable-line
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floor} floors`; // eslint-disable-line
+    return `Evacuate slowly the ${this._floors} floors`; // eslint-disable-line
   }
 }
