ac198af5af96300e5e403b99de8924c1fd741928
Maxwell D. Dorliea <deboy170@gmail.com>
2023-03-27 23:20:24 +0000

    Fixed bug in task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,5 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position > length) {
+  if (position > length || value > 255) {
     throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
