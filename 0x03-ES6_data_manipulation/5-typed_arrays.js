2ff73ccc4adf5e720b828eef51a203be19c814da
Maxwell D. Dorliea <deboy170@gmail.com>
2023-03-29 00:20:36 +0000

    Fixed bug in task 5 solution

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer);
+  const view = new DataView(buffer, 0, length);
   view.setInt8(position, value);
   return buffer;
 }
