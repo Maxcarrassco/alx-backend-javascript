89405f9a594dcdd1c16e300fded6a5cccb954d48
Maxwell D. Dorliea <deboy170@gmail.com>
2023-03-30 00:44:51 +0000

    Added task 4 solution

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,28 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
   return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+   this.firstName = firstName;
+   this.lastName = lastName;
+  };
+
+  workOnHomework(): string {
+   return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+
+}
