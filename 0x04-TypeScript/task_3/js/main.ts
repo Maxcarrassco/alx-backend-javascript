79fbf9a7c3e04557af94ba6e1dc1eb638baad638
Maxwell D. Dorliea <deboy170@gmail.com>
2023-03-31 23:45:28 +0000

    Added task 8 solution

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,68 +1,9 @@
-interface DirectionInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
-}
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud.js';
 
-interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
-
-class Director implements DirectionInterface {
-  workFromHome(): string {
-    return 'Working from home';
-  }
-  getCoffeeBreak(): string {
-    return 'Getting a coffee break';
-  }
-  workDirectorTasks(): string {
-    return 'Getting director tasks';
-  }
-}
-
-class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return 'Cannot work from home';
-  }
-  getCoffeeBreak(): string {
-    return 'Cannot have a break';
-  }
-  workTeacherTasks(): string {
-    return 'Getting to work';
-  }
-}
-
-type  employeeReturn = Teacher | Director;
-type Salary = number | string;
-
-const createEmployee = (salary: Salary): employeeReturn => {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
-  }
-  return new Director();
-};
-
-const isDirector = (employee: employeeReturn): boolean => {
-  if (employee instanceof Director) {
-    return true;
-  }
-  return false;
-}
-
-const executeWork = (employee: any): string => {
-  if (isDirector(employee)) {
-    return employee.workDirectorTasks();
-  }
-  return employee.workTeacherTasks();
-}
-
-type Subjects = 'Math' | 'History';
-
-const teachClass = (subject: Subjects): string => {
-  if (subject === 'Math') {
-    return 'Teaching Math';
-  }
-  return 'Teaching History';
-}
+const obj = {firstName: "Guillaume", lastName: "Salva"};
+CRUD.insertRow(obj)
+const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
+CRUD.updateRow(125, updatedRow);
+CRUD.deleteRow(125);
