da52cc839fc4fe23ccfe07ce6905e616f48e68f9
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-16 14:51:21 +0000

    Fixed bug in 1-stdin.js by checking if data is not null

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (data) => {
-  const inp = data.trim();
-  if (inp) {
-    console.log(`Your name is: ${inp}`);
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
   }
 });
 process.stdin.on('end', () => console.log('This important software is now closing'));
