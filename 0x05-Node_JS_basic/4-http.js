ed015166b9b029a2741c15520a349bf3d48436cf
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-16 18:36:07 +0000

    Created a script implement a simple http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+server.listen(1245, '127.0.0.1', () => console.log('Server Started'));
