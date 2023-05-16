6df33ac2060c5f0e80d1d8165a56ef54348a76fb
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-16 17:21:48 +0000

    Created a script that read from file and print the stats of CS and SWE students

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,24 @@
+const { readFileSync } = require('fs');
+
+const countStudents = (file) => {
+  try {
+    const data = readFileSync(file, 'utf-8');
+    const dataList = data.split('\n').splice(1);
+    const stats = { CS: [], SWE: [] };
+    for (const line of dataList) {
+      const columns = line.split(',');
+      if (columns[3] === 'CS') {
+        stats.CS.push(columns[0]);
+      } else if (columns[3] === 'SWE') {
+        stats.SWE.push(columns[0]);
+      }
+    }
+    for (const [key, value] of Object.entries(stats)) {
+      console.log(`Number of students in ${key}: ${value.length}. ${value.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
