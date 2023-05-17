536fcef1a1cbf4225b65437cbade300021e868ab
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-17 02:28:57 +0000

    Created a well organized express server

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,35 @@
+import readDatabase from '../utils';
+
+const DBFile = (process.argv.length > 2) ? process.argv[2] : '';
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    response.statusCode = 200;
+    response.write('This is the list of our students\n');
+    readDatabase(DBFile).then((data) => {
+      for (const [key, val] of Object.entries(data)) {
+        response.write(`Number of students in ${key}: ${val.length}. List: ${val.join(', ')}\n`);
+      }
+      response.end();
+    }).catch((err) => {
+      response.statusCode = 500;
+      response.end(err.message);
+    });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const major = request.param('major');
+    if (major !== 'CS' && major !== 'SWE') {
+      response.statusCode = 500;
+      response.end('Major parameter must be CS or SWE');
+    }
+    readDatabase(DBFile).then((data) => {
+      response.statusCode = 200;
+      response.end(`List: ${data[major].join(', ')}`);
+    }).catch((err) => {
+      response.statusCode = 500;
+      response.end(err.message);
+    });
+  }
+}
+module.exports = StudentsController;
